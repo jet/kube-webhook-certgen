610410609dc6fb8a1bc4b2bf02f4c0519a5b066a
vas <vasily.sliouniaev@jet.com>
2019-06-06 09:05:15 +0100

    Try prevent build from trying to access everything

--- .cicd/docker.sh
+++ .cicd/docker.sh
@@ -3,5 +3,10 @@ set -eox pipefail
 
 . .cicd/env.sh
 
-docker build . -t $dockerRepo:$vers
+rm -rf dockerbuild > /dev/null
+mkdir dockerbuild
+cp Dockerfile dockerbuild
+mv kube-webhook-certgen dockerbuild
+
+docker build -f dockerbuild/Dockerfile dockerbuild -t $dockerRepo:$vers
 docker run --rm $dockerRepo:$vers version
