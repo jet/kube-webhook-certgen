f34fedf1b488578392c8a5287dfd7f701f27eafe
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2020-10-29 09:19:36 -0300

    Update go to 1.15 (#24)
    * Update go to 1.15
    * Update go dependencies

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -4,15 +4,15 @@ set -eox pipefail
 . .cicd/env.sh
 
 build() {
-  ldflags="-X $mod/core.Version=$vers -X $mod/core.BuildTime=$buildTime"
+  ldflags="-X $mod/core.Version=$vers -X $mod/core.BuildTime=$buildTime -buildid= -w -s"
   docker run --rm \
     -v "$(pwd):/go/src/$mod" \
     -w "/go/src/$mod"  \
     -e CGO_ENABLED=0   \
     -e GOOS=linux      \
     -e GOARCH=$1       \
-    golang:1.14-stretch \
-      go build -mod=vendor -o kube-webhook-certgen-$1 -ldflags "$ldflags"
+    golang:1.15-buster \
+      go build -mod=vendor -o kube-webhook-certgen-$1 -trimpath -ldflags="$ldflags"
 }
 
 build amd64
